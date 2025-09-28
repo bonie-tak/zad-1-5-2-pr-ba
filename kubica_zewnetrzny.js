f9e351ec13923237391bb2b70a5ece434db1f361
bonie-tak <kubbik.szymon@gmail.com>
2025-09-28 20:38:47 +0200

    Update kubica_zewnetrzny.js

--- kubica_zewnetrzny.js
+++ kubica_zewnetrzny.js
@@ -1,3 +1,3 @@
 // Szymon Kubica klasa 3F nr w dzienniku 13
-document.write("ostatnia modyfikacja strony".fontcolor("#00FFFF").strike().fontsize(7) + "<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("#808080").strike().fontsize(5) + "<br>");
 document.write(document.lastModified);
