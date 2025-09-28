3081e613b3e420e8ceadce948152bc131219c240
bonie-tak <kubbik.szymon@gmail.com>
2025-09-28 18:46:55 +0200

    Update kubica_zewnetrzny.js

--- kubica_zewnetrzny.js
+++ kubica_zewnetrzny.js
@@ -1,3 +1,3 @@
-// Stanisław Łybacki klasa 3F nr w dzienniku 16
+// Szymon Kubica klasa 3F nr w dzienniku 13
 document.write("ostatnia modyfikacja strony".fontcolor("#00FFFF").strike().fontsize(7) + "<br>");
 document.write(document.lastModified);
